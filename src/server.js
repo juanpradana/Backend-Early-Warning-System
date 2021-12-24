d604719ac3b4cab255ef16c83d4894b892bbb630
juanpradana <suarahp@gmail.com>
2021-12-24 14:44:52 +0700

    change to https

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const Hapi = require('@hapi/hapi');
+const fs = require('fs');
 const routes = require('./routes');
+
+const tls = {
+  key: fs.readFileSync('127.0.0.1.key'),
+  cert: fs.readFileSync('127.0.0.1.cert'),
+};
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 8000,
+    port: process.env.PORT || 433,
     host: '0.0.0.0',
+    // eslint-disable-next-line object-shorthand
+    tls: tls,
     routes: {
       cors: {
         origin: ['*'],
